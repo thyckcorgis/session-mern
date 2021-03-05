54c3ec70bc5f9794f7e6c5fe3f9a0be7b9b4d6a0
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-05 16:33:04 +0000

    a/b

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,45 @@
 import express from "express";
 import mongoose from "mongoose";
+import session from "express-session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+import MongoStore from "connect-mongo";
 import { userRoutes } from "./routes";
-import { PORT, MONGO_URI } from "./config";
+import { PORT, MONGO_URI, SESS_NAME, SESS_SECRET, SESS_LIFETIME, NODE_ENV } from "./config";
 
 async function main() {
   try {
     await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("MongoDB connected");
 
+    mongoose.connection;
     const app = express();
+    const MongoDBStore = ConnectMongoDBSession(session);
+    const store = new MongoDBStore({
+      uri: MONGO_URI,
+      collection: "session",
+    });
+
+    store.on("error", (err) => console.log(err));
 
     app.disable("x-powered-by");
 
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
+
+    app.use(
+      session({
+        name: SESS_NAME,
+        secret: SESS_SECRET,
+        saveUninitialized: false,
+        resave: false,
+        store,
+        cookie: {
+          sameSite: true,
+          secure: NODE_ENV === "production",
+          maxAge: parseInt(SESS_LIFETIME.toString()),
+        },
+      })
+    );
 
     const apiRouter = express.Router();
     app.use("/api", apiRouter);
