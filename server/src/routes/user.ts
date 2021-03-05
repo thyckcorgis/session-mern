3b10096bd6dc35ae0d01008c55dba2e0a3d345ba
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-05 05:43:20 +0000

    create post route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 
+import User from "../models/user.model";
+import { signUp } from "../validations/user";
+
 const userRoutes = Router();
 
-userRoutes.post("", (req, res) => {
-  res.send(req.body);
+userRoutes.post("", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const { error } = signUp.validate({ username, email, password });
+    if (error) throw error;
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.send({ userId: newUser.id, username });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 export default userRoutes;
