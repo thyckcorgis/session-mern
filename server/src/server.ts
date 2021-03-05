cb1137e3adcd411b4d69be7b199a602da821690f
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-05 17:20:12 +0000

    finish backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import { userRouter } from "./routes";
+import { sessionRouter, userRouter } from "./routes";
 import { PORT, MONGO_URI, SESS_NAME, SESS_SECRET, SESS_LIFETIME, NODE_ENV } from "./config";
 
 async function main() {
@@ -43,6 +43,7 @@ async function main() {
 
     const apiRouter = express.Router();
     app.use("/api", apiRouter);
+    apiRouter.use("/session", sessionRouter);
     apiRouter.use("/users", userRouter);
 
     app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
