bb1397fc5074e9cf98754b4dce4ecb301eab2945
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-05 05:05:01 +0000

    connect mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
 import { userRoutes } from "./routes";
-import { PORT, NODE_ENV } from "./config";
+import { PORT, MONGO_URI } from "./config";
 
-const app = express();
+async function main() {
+  try {
+    await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB connected");
 
-app.disable("x-powered-by");
+    const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    app.disable("x-powered-by");
 
-const apiRouter = express.Router();
-app.use("/api", apiRouter);
-apiRouter.use("/users", userRoutes);
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    const apiRouter = express.Router();
+    app.use("/api", apiRouter);
+    apiRouter.use("/users", userRoutes);
+
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
